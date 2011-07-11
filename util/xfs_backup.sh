7a3d103dcf4c96a2d6ef89f95f69a5d74ce6f824
Pramukta Kumar <prak@mac.com>
2011-07-11 08:45:10 -0400

    untested xfs backup script

---
+++ util/xfs_backup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+### Configuration ###
+
+# Label for the backup set
+LABEL=data1-backup
+MEDIA=ext-array
+# Report progress every $PROGRESS seconds
+PROGRESS=60 
+# Filesystem to backup
+BACKUP_FROM=/dev/sda1
+BACKUP_MOUNT_POINT=/data1
+# Location to store backup
+BACKUP_TO=/mnt/backup
+
+### End Configuration ###
+
+# Decide on a Full or Incremental Backup
+# ()
+LEVEL=1
+if [[ $# > 0 ]]; then
+	if [[ $@ = "full" ]]; then
+		LEVEL=0
+	fi
+fi
+
+OUTPUT_FILE=$BACKUP_TO/$LABEL-$(date +%Y%m%d).$LEVEL
+
+echo $OUTPUT_FILE
+
+xfs_freeze -f $BACKUP_MOUNT_POINT
+xfsdump -p $PROGRESS -L $LABEL -M $MEDIA -l -f $OUTPUT_FILE $BACKUP_FROM
+xfs_freeze -u $BACKUP_MOUNT_POINT
